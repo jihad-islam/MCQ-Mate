063d3dcb9d3680c774eced0d625d07b92db8eb48
jihad-islam <jihadislam.diu@gmail.com>
2026-06-14 12:14:48 +0600

    brevo added

--- backend/mcq_project/settings.py
+++ backend/mcq_project/settings.py
@@ -238,11 +238,10 @@ AUTH_USER_MODEL = 'users.CustomUser'
 # NEW: Email Configuration for Password Reset
 # ==========================================
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-EMAIL_PORT = 465
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
+EMAIL_HOST = 'smtp-relay.brevo.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER') # e.g. your-email@gmail.com
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD') # Google App Password
 
