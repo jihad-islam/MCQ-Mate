ba8b370c2e5c0a02993f9810166f265dddc069d8
jihad-islam <jihadislam.diu@gmail.com>
2026-06-14 11:40:16 +0600

    email port updated

--- backend/mcq_project/settings.py
+++ backend/mcq_project/settings.py
@@ -239,8 +239,10 @@ AUTH_USER_MODEL = 'users.CustomUser'
 # ==========================================
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+# EMAIL_PORT = 587
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER') # e.g. your-email@gmail.com
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD') # Google App Password
 
